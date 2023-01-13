34555e61de9f1340a084443ecb8451a2bc746946
Wildan Kurnia Candra <37010270+user4xn@users.noreply.github.com>
2023-01-13 16:28:25 +0700

    Newest Pinjam Pakai Order

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -69,6 +69,7 @@ class ApprovalController extends Controller
             ->when($request->status, function ($query) use ($request){
                 return $query->where('status', $request->status);
             })
+            ->orderBy('tglpinjam', 'DESC')
             ->get();
         $data = [];
         foreach ($fetch as $pinjam) {
